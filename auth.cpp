2ee10382e3244aefa1fedba8d27592966ee848ee
Ryan Wagner <rkwagner@ucsd.edu>
2014-08-20 14:03:17 -0700

    Commented the head of auth.cpp

---
+++ auth.cpp
@@ -0,0 +1,81 @@
+/*
+ *Author:       Ryan Wagner
+ *Date:         August 20, 2014
+ *Description:  Basic user security program, asks for a name and password
+ *              exiting on 5 unsuccessful attempts.
+ *Input:        Username/Password/Commands
+ *Output:       Login messages.
+ */
+
+#include<iostream>
+#include<fstream>
+#include<string>
+
+using namespace std;
+
+int checkUser(char *name, char *pass);
+void homeScreen(char *name, char *pass);
+
+int main(void){
+  int correct=0,i=0;
+  char name[100],pass[100];
+  while (!correct && i<5){
+    cout<<"User: ";
+    cin>>name;
+    cout<<"Pass: ";
+    cin>>pass;
+    correct = checkUser(name,pass);
+    i++;
+    if(!correct){
+      cout<<"Attempt ("<<i<<") Failed.";
+    }
+    else{
+      homeScreen(name,pass);
+    }
+  }
+  return 0;
+}
+
+int checkUser(char *name, char *pass){
+  ifstream input;
+  input.open("pass.txt");
+  int correct = 0;
+  if(input.is_open()){
+    while(input.good() && !correct){
+      char trueInfo[256];
+      input.getline(trueInfo,256);
+      char trueUser[256];
+      (void)strncpy(trueUser,trueInfo,strrchr(trueInfo,' ')-trueInfo);   
+      char * truePass = strrchr(trueInfo,' ')+1;
+      (strcmp(name,trueUser) || strcmp(pass,truePass))?correct=0:correct=1;
+    }
+    input.close();
+  }
+  else{/*EMPTY*/}
+  return correct;
+}
+
+void homeScreen(char *name, char *pass){
+  cout<<"Welcome back "<<name<<"!\n";
+  char input[256], cmds[256], list[1000];
+  while (strncmp(input,"quit",strlen(input))){
+    cout<<">";
+    cin>>input;
+    ifstream command;
+    command.open("commandList.txt");
+    if(!command){
+      cout<<"File read error."<<endl;
+    }
+    else{
+      while(command.getline(cmds,256)){
+        strcat(list,cmds);
+      }
+      command.close();
+    }
+    if(!strstr(list , input)){
+      cout<<"Command not found."<<endl;
+    }
+    else
+      cout<<"Work done here."<<endl;
+  }
+}
